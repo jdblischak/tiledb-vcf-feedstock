69a14b8778daec0a672c3ffeca236537facb2f41
John Blischak <jdblischak@gmail.com>
2023-02-14 15:44:05 -0500

    A build.sh requires top-level requirements

--- recipe/install-libtiledbvcf.sh
+++ recipe/install-libtiledbvcf.sh
@@ -2,5 +2,15 @@
 
 set -exo pipefail
 
+mkdir libtiledbvcf-build && cd libtiledbvcf-build
+cmake \
+  -DCMAKE_INSTALL_PREFIX:PATH="${PREFIX}" \
+  -DOVERRIDE_INSTALL_PREFIX=OFF \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DFORCE_EXTERNAL_HTSLIB=ON \
+  ../libtiledbvcf
+
+make -j ${CPU_COUNT}
+
 cd libtiledbvcf-build
 make install-libtiledbvcf
