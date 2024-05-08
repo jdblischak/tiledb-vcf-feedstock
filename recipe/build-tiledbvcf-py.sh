02b496c7ea5fc70eaab1c1ae00925e39e013ef84
John Blischak <jdblischak@gmail.com>
2024-05-08 12:34:39 -0400

    Don't pin setuptools to 6.0.1

--- recipe/build-tiledbvcf-py.sh
+++ recipe/build-tiledbvcf-py.sh
@@ -3,4 +3,4 @@
 set -ex
 
 export LIBTILEDBVCF_PATH=$PREFIX/lib/
-$PYTHON -m pip install --no-deps --no-build-isolation -vv apis/python
+$PYTHON -m pip install --no-deps --no-build-isolation -v apis/python
