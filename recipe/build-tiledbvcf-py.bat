ec174709dc67ee0f2d70f299d121b7237cb4f9f1
John Blischak <jdblischak@gmail.com>
2024-05-09 13:46:54 -0400

    Fix path when building Python package

--- recipe/build-tiledbvcf-py.bat
+++ recipe/build-tiledbvcf-py.bat
@@ -1,6 +1,10 @@
 @echo on
 
 set "LIBTILEDBVCF_PATH=%LIBRARY_LIB%"
-cd python-sdist
-%PYTHON% -m pip install --no-deps --no-build-isolation -v .
+
+rem Copy version.py from Python sdist for setuptools-scm
+copy python-sdist\src\tiledbvcf\version.py apis\python\src\tiledbvcf
+if %ERRORLEVEL% neq 0 exit 1
+
+%PYTHON% -m pip install --no-deps --no-build-isolation -v apis\python
 if %ERRORLEVEL% neq 0 exit 1
--- recipe/build-tiledbvcf-py.sh
+++ recipe/build-tiledbvcf-py.sh
@@ -7,4 +7,4 @@ export LIBTILEDBVCF_PATH=$PREFIX/lib/
 # Copy version.py from Python sdist for setuptools-scm
 cp python-sdist/src/tiledbvcf/version.py apis/python/src/tiledbvcf/
 
-$PYTHON -m pip install --no-deps --no-build-isolation -v .
+$PYTHON -m pip install --no-deps --no-build-isolation -v apis/python
